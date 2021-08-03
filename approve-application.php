54056d41ece6a5aaa88c5904c5a879f258cf577b
Eren Geridonmez <65499630+CoderEren@users.noreply.github.com>
2021-08-03 12:47:31 +0100

    Create approve-application.php

---
+++ approve-application.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+$productid = $_POST['productid'];
+
+if (empty($_POST['productid']) || empty($_COOKIE['seller-email'])) {
+	header('location:seller-applications.php');
+} else {
+	$email = $_COOKIE['seller-email'];
+	$buyeremail = $_SESSION['buyer-email'];
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "PASSWORD";
+	$dbname = "fullcashback";
+
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+	// Check connection
+	if (!$conn) {
+	  die("Connection failed: " . mysqli_connect_error());
+	}
+	echo "Connected successfully" . "<br>";
+	
+	$s = "SELECT * FROM applications WHERE productid = '$productid' && useremail = '$buyeremail' && selleremail = '$email'";
+	$result = mysqli_query($conn, $s);
+			
+	if (mysqli_num_rows($result) > 0) {
+	
+		$sql = "UPDATE applications SET purchaseallowed='true' WHERE productid = '$productid' && useremail = '$buyeremail' && selleremail = '$email'";
+
+		if (mysqli_query($conn, $sql)) {
+		  echo "Record updated successfully" . "<br>";
+		} else {
+		  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+	
+	}
+
+	//Close the connection
+	mysqli_close($conn);
+	header('location:seller-applications.php');
+}
+?>
